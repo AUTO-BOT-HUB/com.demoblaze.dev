d157db171cacc1652f480747784e00bfc7b00bad
Manjunath Gaddi <manjunathgaddi@gmail.com>
2026-01-31 12:43:42 +0530

    Day2 New Changes

---
+++ src/test/java/Test/loginValidationTest.java
@@ -0,0 +1,27 @@
+package Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class loginValidationTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        driver.get("https://www.demoblaze.com/");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login2")));
+
+        driver.findElement(By.id("login2")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("logInModalLabel")));
+        driver.findElement(By.id("loginusername")).sendKeys("sagarg@gmail.com");
+        driver.findElement(By.id("loginpassword")).sendKeys("P@ssw0rd@123");
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("logout2")));
+        driver.quit();
+    }
+}
